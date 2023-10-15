a02234932a8576ce0a8ca15dbc77afc7bf0bd5d7
Pavel Štrobl <pavel.strobl@prusa3d.cz>
2023-10-15 17:03:55 +0200

    removal of api key for buddy scrape

--- api.go
+++ api.go
@@ -13,30 +13,20 @@ func getURL(path string, address string) string {
 	return string("http://" + address + "/api/" + path)
 }
 
-func accessBuddyAPI(path string, address string, apiKey string, username string, password string) []byte {
+func accessBuddyAPI(path string, address string, username string, password string) []byte {
 	url := getURL(path, address)
 	var res *http.Response
 	var err error
 	var body []byte
-	if apiKey == "" {
-		client := &http.Client{
-			Transport: &digest.Transport{
-				Username: username,
-				Password: password,
-			},
-		}
-		res, err = client.Get(url)
-		if err != nil {
-			log.Error().Msg(err.Error())
-		}
-	} else {
-		req, _ := http.NewRequest("GET", url, nil)
-		client := &http.Client{}
-		req.Header.Add("X-Api-Key", apiKey)
-		res, err = client.Do(req)
-		if err != nil {
-			log.Error().Msg(err.Error())
-		}
+	client := &http.Client{
+		Transport: &digest.Transport{
+			Username: username,
+			Password: password,
+		},
+	}
+	res, err = client.Get(url)
+	if err != nil {
+		log.Error().Msg(err.Error())
 	}
 	if err == nil {
 		body, err = io.ReadAll(res.Body)
